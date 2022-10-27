b57e8f85f02e23bf00fb7ca66f48d457896a69cb
Guilherme Rodrigues Martins <guilherme.martinseng@hotmail.com>
2022-10-26 21:38:36 -0300

    a

--- bin/pw_back_end.dart
+++ bin/pw_back_end.dart
@@ -1,17 +1,26 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:mongo_dart/mongo_dart.dart' as dart_mongo;
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
-final String address = 'localhost';
+final String address = '0.0.0.0';
 
 void main(List<String> arguments) async {
+  var env = Platform.environment;
+  var port = env.entries.firstWhere((element) => element.key == 'PORT',
+      orElse: () => MapEntry('PORT', '8080'));
+
   final app = Router()
     ..get('/', homeHandler)
     ..post('/login', loginHandler);
-  await io.serve(app, address, 8080);
+  var server = await io.serve(app, address, int.parse(port.value));
+
+  server.autoCompress = true;
+
+  print('Ta rodando em $address:${port.value}');
 }
 
 loginHandler(Request request) {
